d71b3aa439c03d2bf29cbe676ee5834b0d708627
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-22 16:37:23 +0600

    add import & export

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
 const app = express();
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config();
-const mongoose = require("mongoose");
+dotenv.config()
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -11,19 +12,10 @@ app.use(cors());
 app.use(express.json());
 // ---------------------------
 
-// const uri =
-//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efjve.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
-
 // async await function
 const connect = async () => {
   try {
     mongoose.connect(process.env.MONGO);
-    
   } catch (error) {
     // console.log("MongoDB To disconnected!");
     throw error;
@@ -37,23 +29,6 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB Disconnected!");
 });
-
-// async function run() {
-//   // try catch finally
-//   try {
-//     await client.connect();
-//         console.log("MongoDB To Connected.");
-//     // const userCollection = client.db("foodExpress").collection("user");
-//   }  catch (error) {
-//         console.log("MongoDB To disconnected.");
-//         throw error;
-//       }
-//     finally {
-//   }
-// }
-// // call function catch (console dir)
-// run().catch(console.dir);
-// --------------------------------------------
 
 // http://localhost:5000/
 app.get("/", (req, res) => {

20216d2e1cd133dc2b3ed517c427de0c685afaec
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-22 15:49:26 +0600

    mongodb conneted

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -9,21 +11,48 @@ app.use(cors());
 app.use(express.json());
 // ---------------------------
 
+// const uri =
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efjve.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+
 // async await function
-async function run() {
+const connect = async () => {
+  try {
+    mongoose.connect(process.env.MONGO);
     
-    // try catch finally
-    try {
-      await client.connect();
-      console.log("Database Connected YaY!")
-      // const userCollection = client.db("foodExpress").collection("user");
-        
-    } finally {
-        
-    }
-}
-// call function catch (console dir)
-run().catch(console.dir)
+  } catch (error) {
+    // console.log("MongoDB To disconnected!");
+    throw error;
+  }
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB Connected!");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB Disconnected!");
+});
+
+// async function run() {
+//   // try catch finally
+//   try {
+//     await client.connect();
+//         console.log("MongoDB To Connected.");
+//     // const userCollection = client.db("foodExpress").collection("user");
+//   }  catch (error) {
+//         console.log("MongoDB To disconnected.");
+//         throw error;
+//       }
+//     finally {
+//   }
+// }
+// // call function catch (console dir)
+// run().catch(console.dir);
 // --------------------------------------------
 
 // http://localhost:5000/
@@ -32,5 +61,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  connect();
+  console.log(`Connected To backend. Port: ${port}`);
 });

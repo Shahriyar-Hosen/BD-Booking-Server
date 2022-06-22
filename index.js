803382eb0da7aedc3e89c37fc0f5a39b0dc2d174
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-22 16:45:07 +0600

    auth route done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import authRoute from './routes/auth.js'
 
 const app = express();
 dotenv.config()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/auth", authRoute)
 // ---------------------------
 
 // async await function
@@ -31,9 +33,9 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // http://localhost:5000/
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port, () => {
   connect();

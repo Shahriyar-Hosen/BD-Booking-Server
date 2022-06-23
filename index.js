958182e0598021ba737bc3f5aec4990524f7d789
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-06-23 12:38:49 +0600

    hotels post mathod done

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import authRoute from './routes/auth.js'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoute from "./routes/auth.js";
+import hotelRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
 
 const app = express();
-dotenv.config()
-const port = process.env.PORT || 5000;
+dotenv.config();
+const port = process.env.PORT || 8800;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRoute)
+
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelRoute);
+app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
 // ---------------------------
 
 // async await function
 const connect = async () => {
   try {
     mongoose.connect(process.env.MONGO);
+    console.log("Connected To MongoDB!");
   } catch (error) {
     // console.log("MongoDB To disconnected!");
     throw error;
   }
 };
 
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB Connected!");
-});
+// mongoose.connection.on("connected", () => {
+//   console.log("mongoDB Connected!");
+// });
 
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB Disconnected!");
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,9 +1,28 @@
-import express from 'express'
+import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, this is auth endpoint");
+// CREATE
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  console.log("req: ", req);
+  console.log("new Hotel: ", newHotel);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).send(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
-export default router
+// UPDATE
+// DELETE
+// GET
+// GET ALL
+// router.get("/", (req, res) => {
+//   res.send("Hello, this is hotels endpoint");
+// });
+
+export default router;

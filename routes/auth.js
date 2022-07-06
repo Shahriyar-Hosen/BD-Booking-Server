244a9920f8a78d3438963f6f5f04941ad678ed09
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-06 15:40:41 +0600

    user creat done

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from "express";
+import { register } from "../Controllers/Auth.js";
 
 const router = express.Router();
 
@@ -6,8 +7,6 @@ router.get("/", (req, res) => {
   res.send("Hello, this is auth endpoint");
 });
 
-router.get("/register", (req, res) => {
-  res.send("Hello, this is auth endpoint");
-});
+router.post("/register", register);
 
-export default router
+export default router;

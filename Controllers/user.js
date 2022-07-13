8eae3d3c7d489ae4950eb9890567d5a7f7e24b37
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-13 16:26:02 +0600

    user api add

---
+++ Controllers/user.js
@@ -0,0 +1,49 @@
+import User from "../models/User.js";
+
+// UPDATE
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+// ===================================================
+
+// DELETE
+export const deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+// ===================================================
+
+// GET
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+// ===================================================
+
+// GET ALL
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+// ===================================================

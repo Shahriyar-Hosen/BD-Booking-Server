3f585c51f2f1335d498ae90a593e1c2fc586ca32
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-13 22:08:06 +0600

    checkauthentication

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,18 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../Controllers/user";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../Controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/VerifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
+});
+
 
 //UPDATE
 router.put("/:id", updateUser);

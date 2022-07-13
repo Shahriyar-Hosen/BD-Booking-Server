d5571836b26ba714e61d1f69e3abb3cb26d381c5
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-13 16:30:11 +0600

    user root add

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../Controllers/user";
 
 const router = express.Router();
 
+//UPDATE
+router.put("/:id", updateUser);
+// =====================================
 
+//DELETE
+router.delete("/:id", deleteUser);
+// =====================================
 
-export default router
+//GET
+router.get("/:id", getUser);
+// =====================================
+
+//GET ALL
+router.get("/", getUsers);
+// =====================================
+
+export default router;

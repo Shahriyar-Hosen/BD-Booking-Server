ba438f961fea4e16495e03b5fc8bbe72f48017be
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-15 11:01:12 +0600

    create room rootes

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,7 +1,11 @@
-import express from 'express'
+import express from "express";
+import { createRoom } from "../Controllers/room";
+import { verifyAdmin } from "../utils/VerifyToken";
 
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+// ===============================================
 
-
-export default router
+export default router;

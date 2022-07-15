c32f0aa9970962ffd11d0ff0d374c6951cead47f
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-15 10:14:15 +0600

    Controllers update Room

--- Controllers/room.js
+++ Controllers/room.js
@@ -22,3 +22,35 @@ export const createRoom = async (req, res, next) => {
   }
 };
 // ====================================================
+
+//UPDATE
+export const updateRoom = async (req, res, next) => {
+    try {
+      const updatedRoom = await Room.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedRoom);
+    } catch (err) {
+      next(err);
+    }
+  };
+  // ----------------------------------------------------
+  
+  export const updateRoomAvailability = async (req, res, next) => {
+    try {
+      await Room.updateOne(
+        { "roomNumbers._id": req.params.id },
+        {
+          $push: {
+            "roomNumbers.$.unavailableDates": req.body.dates
+          },
+        }
+      );
+      res.status(200).json("Room status has been updated.");
+    } catch (err) {
+      next(err);
+    }
+  };
+  // ===============================================================

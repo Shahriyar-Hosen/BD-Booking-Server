ce5ab3f6cbdbded36baf8301beee3de4f16c08c2
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-15 10:54:52 +0600

    controllers delete room

--- Controllers/room.js
+++ Controllers/room.js
@@ -54,3 +54,23 @@ export const updateRoom = async (req, res, next) => {
     }
   };
   // ===============================================================
+  
+  //DELETE
+  export const deleteRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    try {
+      await Room.findByIdAndDelete(req.params.id);
+      try {
+        await Hotel.findByIdAndUpdate(hotelId, {
+          $pull: { rooms: req.params.id },
+        });
+      } catch (err) {
+        next(err);
+      }
+      res.status(200).json("Room has been deleted.");
+    } catch (err) {
+      next(err);
+    }
+  };
+  // =====================================================
+  

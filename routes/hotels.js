1d56f56625718012c999cc8b1610ed387d506160
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-07-16 16:30:58 +0600

    get find by id

--- routes/hotels.js
+++ routes/hotels.js
@@ -25,7 +25,7 @@ router.delete("/:id", verifyAdmin, deleteHotel);
 // ================================
 
 // GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 // ================================
 
 // GET ALL
